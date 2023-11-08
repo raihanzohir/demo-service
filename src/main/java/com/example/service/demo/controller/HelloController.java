5dc6e03f0bb0a1aa0cbe5f52a921bb3b8c3754a5
Md. Zohir Raihan <zohir@erainfotechbd.com>
2023-11-08 10:14:04 +0600

    Added Hello Controller and Modified application.properties

---
+++ src/main/java/com/example/service/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.service.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> sayHello() {
+        return new ResponseEntity<>("Hello!", HttpStatus.OK);
+    }
+}
